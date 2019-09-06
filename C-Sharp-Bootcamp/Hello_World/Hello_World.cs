d45ab2f0aae17248d9909473db6a349179f487f7
obit91 <ohabel@gmail.com>
2019-09-06 19:56:31 +0300

    Implemented a basic Hello World program.

---
+++ C-Sharp-Bootcamp/Hello_World/Hello_World.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_Bootcamp.Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
