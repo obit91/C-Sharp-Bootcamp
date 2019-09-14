ccac17eb5b0b5f7c5fef05290794c1168412ff4d
obit91 <ohabel@gmail.com>
2019-09-14 20:36:14 +0300

    Basic String.Format(...) examples.

---
+++ C-Sharp-Bootcamp/Demos/StringFormatDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Bootcamp.Demos
+{
+    class StringFormatDemo
+    {
+        internal static void InnerMain()
+        {
+            //methods returning string { "Hello", "From", "Tutorials", "Point" };
+            string[] sarray = { "Hello", "From", "Tutorials", "Point" };
+            string message = String.Join(" ", sarray);
+            Console.WriteLine("Message: {0}", message);
+
+            //formatting method to convert a value
+            DateTime waiting = new DateTime(2012, 10, 10, 17, 58, 1);
+            string chat = String.Format("Message sent at {0:t} on {0:D}", waiting);
+            Console.WriteLine("Message: {0}", chat);
+        }
+    }
+}
